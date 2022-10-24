e7d4d476244bd9b6bb52449a9830bb42cb83b087
kientux <kiennt@sapo.vn>
2022-10-24 18:19:28 +0700

    Fix access Bundle from pod

--- Sources/CZPickerView/Extensions/Bundle+Exts.swift
+++ Sources/CZPickerView/Extensions/Bundle+Exts.swift
@@ -8,11 +8,30 @@
 import Foundation
 
 extension Bundle {
+    private static let bundleName = "CZPickerView"
+    
     static var resource: Bundle {
         #if SWIFT_PACKAGE
         return Bundle.module
         #else
-        return Bundle(for: CZPickerView.self)
+        // Get the bundle containing the binary with the current class.
+        // If frameworks are used, this is the frameworks bundle (.framework),
+        // if static libraries are used, this is the main app bundle (.app).
+        let myBundle = Bundle(for: CZPickerView.self)
+
+        // Get the URL to the resource bundle within the bundle
+        // of the current class.
+        guard let resourceBundleURL = myBundle.url(
+            forResource: bundleName, withExtension: "bundle") else {
+            return myBundle
+        }
+
+        // Create a bundle object for the bundle found at that URL.
+        guard let resourceBundle = Bundle(url: resourceBundleURL) else {
+            return myBundle
+        }
+        
+        return resourceBundle
         #endif
     }
 }
